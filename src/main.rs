f687081aaf4be6bc0a667b596e2599c0f58613b9
Koki Takahashi <hakatasiloving@gmail.com>
2018-02-25 00:08:22 +0900

    Implement Piese struct and Board#print

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,31 @@ struct Board {
     player: bool,
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+struct Piece(u8);
+
+impl Board {
+    fn get_grid(&self, x: u8, y: u8) -> Piece {
+        Piece(((self.grids >> ((y * 3 + x) * 5)) & 0b11111) as u8)
+    }
+
+    fn print(&self) {
+        println!("grids");
+
+        for y in 0..3 {
+            for x in 0..3 {
+                println!("({}, {}): {:?}", x, y, self.get_grid(x, y));
+            }
+        }
+    }
+}
+
 fn main() {
     let board = Board {
-        grids: 0b0000000000000000000_00000_00000_00000_00000_00000_00000_00000_00000_00000,
+        grids: 0b0000000000000000000_00110_00101_00110_00101_00100_00011_00010_00001_00000,
         hands: 0b00000_000_000_0000_0000_0000_0000_00000,
         player: true,
     };
 
-    println!("{:?}", board);
+    board.print();
 }
