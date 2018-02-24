7343e4c78a2cbbf1ac60dc41003c398cb1312253
Koki Takahashi <hakatasiloving@gmail.com>
2018-02-25 02:27:05 +0900

    Add more tests for hand_data_to_hand_info

--- src/util.rs
+++ src/util.rs
@@ -11,18 +11,28 @@ pub struct HandInfo {
     pub second: u8,
 }
 
+fn sqrt_ceil(n: u16) -> u16 {
+    let sqrt_floor = n.integer_sqrt();
+    if sqrt_floor * sqrt_floor == n {
+        return sqrt_floor;
+    } else {
+        return sqrt_floor + 1;
+    }
+}
+
 // 例えばサイズ n (= max_pieces + 1) = 3 に対して
 //     [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (2, 0)]
 // のようになるような (a, b) の配列に対してインデックス (= hand_data) からペアを取得する処理
-// a = i 未満となる要素の数Nは N = (2n - i) * i / 2
-// ここからiを解くと i = n - sqrt(n^2 - 2N)
+// a = i 未満となる要素の数Nは N = (2n - i + 1) * i / 2
+// ここからiを解くと i = (2n + 1 - sqrt(4n^2 + 4n + 1 - 8N)) / 2
 // つまりNにインデックスを代入して求められるiを切り捨てたものがaの値となる。
 pub fn hand_data_to_hand_info(hand_data: u8, max_pieces: u8) -> HandInfo {
-    let size = max_pieces + 1;
-    let i = size - (size * size - hand_data * 2).integer_sqrt();
-    let N = (size * 2 - i) * i / 2;
+    assert!(hand_data < (max_pieces + 1) * (max_pieces + 2) / 2);
+    let size: u16 = (max_pieces + 1) as u16;
+    let i: u16 = (2 * size + 1 - sqrt_ceil(4 * size * size + 4 * size + 1 - 8 * (hand_data as u16))) / 2;
+    let N: u16 = (size * 2 - i + 1) * i / 2;
     return HandInfo {
-        first: i,
-        second: hand_data - N,
+        first: i as u8,
+        second: (hand_data as u16 - N) as u8,
     };
 }
--- tests/util.rs
+++ tests/util.rs
@@ -5,4 +5,36 @@ use nine_grids_shogi_analyzer::util::{hand_data_to_hand_info, HandInfo};
 #[test]
 fn it_works() {
     assert_eq!(hand_data_to_hand_info(0, 2), HandInfo {first: 0, second: 0});
+    assert_eq!(hand_data_to_hand_info(1, 2), HandInfo {first: 0, second: 1});
+    assert_eq!(hand_data_to_hand_info(2, 2), HandInfo {first: 0, second: 2});
+    assert_eq!(hand_data_to_hand_info(3, 2), HandInfo {first: 1, second: 0});
+    assert_eq!(hand_data_to_hand_info(4, 2), HandInfo {first: 1, second: 1});
+    assert_eq!(hand_data_to_hand_info(5, 2), HandInfo {first: 2, second: 0});
+
+    assert_eq!(hand_data_to_hand_info(0, 4), HandInfo {first: 0, second: 0});
+    assert_eq!(hand_data_to_hand_info(1, 4), HandInfo {first: 0, second: 1});
+    assert_eq!(hand_data_to_hand_info(2, 4), HandInfo {first: 0, second: 2});
+    assert_eq!(hand_data_to_hand_info(3, 4), HandInfo {first: 0, second: 3});
+    assert_eq!(hand_data_to_hand_info(4, 4), HandInfo {first: 0, second: 4});
+    assert_eq!(hand_data_to_hand_info(5, 4), HandInfo {first: 1, second: 0});
+    assert_eq!(hand_data_to_hand_info(10, 4), HandInfo {first: 2, second: 1});
+    assert_eq!(hand_data_to_hand_info(11, 4), HandInfo {first: 2, second: 2});
+    assert_eq!(hand_data_to_hand_info(12, 4), HandInfo {first: 3, second: 0});
+    assert_eq!(hand_data_to_hand_info(13, 4), HandInfo {first: 3, second: 1});
+    assert_eq!(hand_data_to_hand_info(14, 4), HandInfo {first: 4, second: 0});
+
+    assert_eq!(hand_data_to_hand_info(0, 7), HandInfo {first: 0, second: 0});
+    assert_eq!(hand_data_to_hand_info(1, 7), HandInfo {first: 0, second: 1});
+    assert_eq!(hand_data_to_hand_info(2, 7), HandInfo {first: 0, second: 2});
+    assert_eq!(hand_data_to_hand_info(3, 7), HandInfo {first: 0, second: 3});
+    assert_eq!(hand_data_to_hand_info(4, 7), HandInfo {first: 0, second: 4});
+    assert_eq!(hand_data_to_hand_info(5, 7), HandInfo {first: 0, second: 5});
+    assert_eq!(hand_data_to_hand_info(6, 7), HandInfo {first: 0, second: 6});
+    assert_eq!(hand_data_to_hand_info(7, 7), HandInfo {first: 0, second: 7});
+    assert_eq!(hand_data_to_hand_info(8, 7), HandInfo {first: 1, second: 0});
+    assert_eq!(hand_data_to_hand_info(31, 7), HandInfo {first: 5, second: 1});
+    assert_eq!(hand_data_to_hand_info(32, 7), HandInfo {first: 5, second: 2});
+    assert_eq!(hand_data_to_hand_info(33, 7), HandInfo {first: 6, second: 0});
+    assert_eq!(hand_data_to_hand_info(34, 7), HandInfo {first: 6, second: 1});
+    assert_eq!(hand_data_to_hand_info(35, 7), HandInfo {first: 7, second: 0});
 }
