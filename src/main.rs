876add22cb69bc13f36b080834614c92551d0125
Koki Takahashi <hakatasiloving@gmail.com>
2018-02-25 01:11:56 +0900

    Implement util::hand_data_to_hand_info

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-#![feature(test)]
+#![feature(non_ascii_idents)]
 
-extern crate fnv;
-extern crate test;
+extern crate integer_sqrt;
 
-use fnv::FnvHashMap;
+use std::vec::Vec;
+use integer_sqrt::IntegerSquareRoot;
+
+mod util;
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 struct Board {
@@ -23,21 +25,47 @@ impl Board {
         Piece(((self.grids >> ((y * 3 + x) * 5)) & 0b11111) as u8)
     }
 
+    fn get_hands(&self, player: u8) -> Vec<Piece> {
+        let mut hands: Vec<Piece> = Vec::with_capacity(27);
+
+        let mut temp_hands = self.hands;
+        for i in 0..7 {
+            let size = match i {
+                0 | 1 => 3,
+                2 | 3 | 4 | 5  => 4,
+                6 => 5,
+                _ => panic!(),
+            };
+            let max_pieces = match i {
+                0 | 1 => 2,
+                2 | 3 | 4 | 5 => 3,
+                6 => 7,
+                _ => panic!(),
+            };
+            let hands = util::hand_data_to_hand_info((temp_hands & ((1 << size) - 1)) as u8, max_pieces);
+            temp_hands >>= size;
+        }
+
+        return hands;
+    }
+
     fn print(&self) {
-        println!("grids");
+        println!("== grids ==");
 
         for y in 0..3 {
             for x in 0..3 {
                 println!("({}, {}): {:?}", x, y, self.get_grid(x, y));
             }
         }
+
+        println!("{:?}", self.get_hands(0));
     }
 }
 
 fn main() {
     let board = Board {
         grids: 0b0000000000000000000_00110_00101_00110_00101_00100_00011_00010_00001_00000,
-        hands: 0b00000_000_000_0000_0000_0000_0000_00000,
+        hands: 0b000000000_00000_0000_0000_0000_0000_000_000,
         player: true,
     };
 
---
+++ src/util.rs
@@ -0,0 +1,27 @@
+extern crate integer_sqrt;
+
+use integer_sqrt::IntegerSquareRoot;
+
+// ある駒についての先手後手の持ち駒の数を表す
+pub struct HandInfo {
+    // 先手
+    first: u8,
+    // 後手
+    second: u8,
+}
+
+// 例えばサイズ n (= max_pieces + 1) = 3 に対して
+//     [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (2, 0)]
+// のようになるような (a, b) の配列に対してインデックス (= hand_data) からペアを取得する処理
+// a = i 未満となる要素の数Nは N = (2n - i) * i / 2
+// ここからiを解くと i = n - sqrt(n^2 - 2N)
+// つまりNにインデックスを代入して求められるiを切り捨てたものがaの値となる。
+pub fn hand_data_to_hand_info(hand_data: u8, max_pieces: u8) -> HandInfo {
+    let size = max_pieces + 1;
+    let i = size - (size * size - hand_data * 2).integer_sqrt();
+    let N = (size * 2 - i) * i / 2;
+    return HandInfo {
+        first: i,
+        second: hand_data - N,
+    };
+}
