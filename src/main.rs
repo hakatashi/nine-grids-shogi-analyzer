b5ade79cd618e3a7b9be33e9a58bd5f218aa84e5
Koki Takahashi <hakatasiloving@gmail.com>
2018-02-24 23:48:56 +0900

    Implement Board structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#![feature(test)]
+
+extern crate fnv;
+extern crate test;
+
+use fnv::FnvHashMap;
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+struct Board {
+    // マスの状態
+    grids: u64,
+    // 持ち駒
+    hands: u32,
+    // 手番 (先手/後手)
+    player: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let board = Board {
+        grids: 0b0000000000000000000_00000_00000_00000_00000_00000_00000_00000_00000_00000,
+        hands: 0b00000_000_000_0000_0000_0000_0000_00000,
+        player: true,
+    };
+
+    println!("{:?}", board);
 }
